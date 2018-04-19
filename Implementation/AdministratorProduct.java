21e57c2ae29a5e6ef0a2bae82c5fea7005cafa01
sakhors <sakhors@iu.edu>
2018-04-19 12:38:01 -0400

    Browse fields and purchase problem has been fixed

--- Implementation/AdministratorProduct.java
+++ Implementation/AdministratorProduct.java
@@ -27,11 +27,20 @@ public class AdministratorProduct implements ProductInterface, Serializable {
 			while (rs.next())  {
 				int id = rs.getInt("productId");
 				String productName = rs.getString("name"); 
+				float productPrice = rs.getFloat("price");
+				int productQuantity = rs.getInt("quantity");
+				String productDescription = rs.getString("description");
 				str.append(id);
-				str.append(" ");
+				str.append(" Name: ");
 				str.append(productName);
+				str.append(" Price: ");
+				str.append(productPrice);
+				str.append(" Quantity: ");
+				str.append(productQuantity);
+				str.append(" Description: ");
+				str.append(productDescription);
 				str.append("\n");
-				System.out.println("ID: " + id + "\t" + "Name: " + productName);				
+				System.out.println("ID: " + id + "\t" + "Name: " + productName + "\t" + "Price: " + productPrice + "\t" + "Quantity: " + productQuantity + "\t" + "Description: " + productDescription);			
 			}
 			
 		}catch(Exception e){
@@ -83,7 +92,7 @@ public class AdministratorProduct implements ProductInterface, Serializable {
 		try{
 			String stm = String.format("INSERT INTO product(name, description, quantity, price, status) VALUES (' %s','%s',%s,%s,'active');",name,description,quantity,price);
 			System.out.println(stm);
-			dbManager.updateMyRecord(stm);
+			int result = dbManager.updateMyRecord(stm);
 			commandStatus = "Product has been added successfully!";
 		}catch(Exception e){
 			commandStatus = "Please try again later!";
